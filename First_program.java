d2470939b4734da9f81f4e9fd0e0465d6e92c567
pavanikaragi <pavanikaragi@gmail.com>
2023-06-14 16:15:08 +0530

    initial commit

---
+++ First_program.java
@@ -0,0 +1,10 @@
+package javatraining;
+
+public class First_program {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Effigo....");
+		System.out.println("JAVA TRAINING...");
+	}
+
+}
---
+++ datatypedemo.java
@@ -0,0 +1,22 @@
+package javatraining;
+
+public class datatypedemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 100;
+		double d = 15.5;
+		char c = 'A';
+		boolean b = false;
+
+		String s = "welcome";
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(d);
+
+	}
+
+}
